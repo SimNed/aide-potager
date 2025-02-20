d3347365b183db4a0a5b6cd486e45673d26e75db
SimNed <simon.nedjari@outlook.com>
2025-02-20 19:57:25 +0100

    other test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Section from "@/components/Section";
 import { Button } from "@/components/shadcn-ui/button";
+import helloWorld from "@/lib/db";
 
 export default async function HomePage() {
+  const dbHello = await helloWorld();
   return (
     <Section
       variant="lg"
       className="h-full flex flex-col justify-center items-center space-y-28 text-5xl font-bold"
     >
+      return <p>DBHELLO : {JSON.stringify(dbHello)}</p>;
       <h1 className="my-4">Aide potager</h1>
       <Button className="rounded-full">commencer</Button>
     </Section>
